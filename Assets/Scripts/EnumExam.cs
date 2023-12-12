ff1c860fd62304bc0428b6c1176f0bdba0e601ea
BrianAsi4n <146366610+BrianAsi4n@users.noreply.github.com>
2023-12-12 20:30:26 +0700

    import lesson3
    update lesson3

---
+++ Assets/Scripts/EnumExam.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    MainMenu,
+    InGame,
+    Paussed,
+    GameOver
+}
+
+
+public class EnumExam : MonoBehaviour
+{
+    public GameState CurrentState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentState = GameState.MainMenu;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CurrentState == GameState.MainMenu)
+        {
+            // hien thi UI Menu
+
+        }else if(CurrentState == GameState.InGame)
+        {
+            //ẩn UI Menu 
+            //hiện In Game UI
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public enum TargetEnum
+{
+    TopLeft,
+    TopRight,
+    Bottomleft,
+    BottomRight,
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    public Transform topLeftTransform;
+    public Transform topRightTransform;
+    public Transform bottomLeftTransform;
+    public Transform bottomRightTransform;
+
+    private TargetEnum nextTarget = TargetEnum.TopLeft; // gan trang thai
+    private Transform currentTarget;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTarget = topLeftTransform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Hàm Update được gọi mỗi frame
+        Vector3 targetPosition = currentTarget.position;
+        Vector3 moveDirection = targetPosition - transform.position;
+        float distance = moveDirection.magnitude;//tính khỏang cách giữa hai điểm
+        if(distance > 0.1f)
+        {
+            //khi chua den diem tieo theo van tieo tuc di chuyen
+            transform.position = Vector3.MoveTowards(transform.position,currentTarget.position,
+                                                       Speed* Time.deltaTime);
+        }
+        else
+        {
+            //CHuyển trạng thái target
+            setNextTarget(nextTarget);
+        }
+        //Thay đổi góc quy theo hướng target
+        Vector3 direction = currentTarget.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(direction,Vector3.up);
+        transform.rotation = targetRotation;
+    }
+    void setNextTarget(TargetEnum target)
+    {
+        switch (target)
+        {
+            case TargetEnum.TopLeft:
+                currentTarget = topLeftTransform;
+                nextTarget = TargetEnum.TopRight;
+                break;
+            case TargetEnum.TopRight:
+                currentTarget = topRightTransform;
+                nextTarget = TargetEnum.Bottomleft;
+                break;
+            case TargetEnum.Bottomleft:
+                currentTarget = bottomLeftTransform;
+                nextTarget = TargetEnum.BottomRight;
+                break;
+            case TargetEnum.BottomRight:
+                currentTarget = bottomRightTransform;
+                nextTarget = TargetEnum.TopLeft;
+                break;
+        }
+    }
+   
+}
